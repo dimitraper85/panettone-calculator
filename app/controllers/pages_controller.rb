04cfd296eb5344b90f3a0ccab27068c74fd4d120
Dimitra Perissiou <dimitraper85@gmail.com>
2024-11-01 18:27:16 +0100

    calculator function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,27 @@
 class PagesController < ApplicationController
   def calculator
-    @flour = params[:flour_grams]
-    @water = params[:water_grams]
+    @dough_weight = params[:dough_weight].to_f
+    factor = @dough_weight / 566 * 100
+    ingredients = {
+      flour_1: 1.0,
+      flour_2: 0.25,
+      water_1: 0.5,
+      water_2: 0.07,
+      yolk_1: 0.25,
+      yolk_2: 0.45,
+      starter_1: 0.25,
+      butter_1: 0.25,
+      butter_2: 0.69,
+      sugar_1: 0.31,
+      sugar_2: 0.19,
+      honey_2: 0.125,
+      salt_2: 0.015,
+      candied_fruit_2: 1.3
+    }
+    @ingredients = {}
+    ingredients.each do |ingredient, value|
+      @ingredients[ingredient] = value * factor
+    end
   end
 
   def home
